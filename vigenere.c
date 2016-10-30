a54ce11e513d02fc2e56ce106409d64a05eb4b1c
Pavel Novitskiy <pasha.6912@gmail.com>
2016-10-30 11:11:37 +0200

    Changes which improve quality of code
    I added some comments and several changes to make your code smaller and clearer.

--- vigenere.c
+++ vigenere.c
@@ -2,41 +2,36 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-int main(int argc, string argv[])
-{
-    if (argc != 2)
-    {
-        printf("Try again.");
+int main(int argc, string argv[]){
+    // The number of command line args submitted was incorrect.
+    if (argc != 2){
+        printf("Try again.\n");
         return 1;
     }
     string key = argv[1];
     long SumOfElementsKey = strlen(key);
-    for(int index = 0; index < SumOfElementsKey; index++) 
-    {
-        if (!isalpha(key[index]))
-        {
-            printf("Try again.");
+    // Check if keyword is correct.
+    for(int index = 0; index < SumOfElementsKey; index++) {
+        if (!isalpha(key[index])){
+            printf("Try again.\n");
             return 1;
         }
     }
-    printf("enter the text \n");
+    printf("enter the text\n");
     string text = GetString();
-    if(text == NULL) 
-    {
-        printf("Try again.");
+    // Check if one keyword is empty.
+    if(text == NULL){
+        printf("Try again.\n");
         return 1;
     }
-    for (int i = 0, n = 0; i < strlen(text); i++) 
-    {
-        if (isalpha(text[i])) 
-        {
-            if (isupper(text[i]))
-               printf("%c", ((((text[i] - 'A') + ((toupper(key[n++ % SumOfElementsKey]))-'A')%26) % 26) + 'A'));
-            if (islower(text[i]))
-               printf("%c", ((((text[i] - 'a') + ((tolower(key[n++ % SumOfElementsKey]))-'a')%26) % 26) + 'a'));
-        } 
-        else
-        printf("%c", text[i]);
+    // Keyword is correct, continue encipherment.
+    for (int i = 0, n = 0; i < strlen(text); i++){
+        if (isalpha(text[i]))
+        else if (isupper(text[i]))
+             printf("%c", ((((text[i] - 'A') + ((toupper(key[n++ % SumOfElementsKey]))-'A')%26) %26) + 'A'));
+        else if (islower(text[i]))
+             printf("%c", ((((text[i] - 'a') + ((tolower(key[n++ % SumOfElementsKey]))-'a')%26) %26) + 'a'));
+        else printf("%c", text[i]);
     }
     printf("\n");
     return 0;
